0f8c965d698209a03a4f93aca4a090330064effc
FrancesDiaz <115190000+francesvdiaz@users.noreply.github.com>
2023-02-22 23:31:17 -0800

    passed first test. Creating new recipe object

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,8 @@ function Recipe(name, ingredients, cookingTime, instructions) {
   this.instructions = instructions;
 }
 
-const recipeForm = document.getElementById("recipe-form");
-
-const recipeFormSubmission = recipeForm.addEventListener("submit", function(event) {
-  event.preventDefault();
-
+//This will add the Recipe properties to a new object and oberve its new qualities
+Recipe.prototype.createRecipe = function() {
   const recipeName = document.getElementById("recipe-name").value;
   const recipeIngredients = document.getElementById("recipe-ingredients").value;
   const recipeInstructions = document.getElementById("recipe-instructions").value;
@@ -18,8 +15,15 @@ const recipeFormSubmission = recipeForm.addEventListener("submit", function(even
   const recipe = new Recipe(recipeName,recipeIngredients,recipeInstructions,recipeCookingTime);
 
   console.log(recipe);
-});
+};
 
-window.addEventListener("load", function() {
-  document.getElementById("button").addEventListener("click",recipeFormSubmission);
-}); 
+Recipe.prototype.submitForm = function(event) {
+  event.preventDefault();
+  this.createRecipe();
+};
+
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  const recipeForm = document.getElementById("recipe-form");
+  recipeForm.addEventListener("submit",Recipe.prototype.submitForm.bind(new Recipe()));
+});
